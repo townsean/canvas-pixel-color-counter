b3b4e331497e9f0f42a19f04b93fbf86222aa68a
Ashley Grenon <ashley.grenon@gmail.com>
2019-10-23 12:10:02 -0500

    Added logic to count the number of pixels per unqiue color

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+/**
+ * Event listener for when the file upload has been updated
+ */
 document.getElementById("image").addEventListener('change', (e) => {
     this.loadImage(e.target.files[0]);
 }, false);
 
+/**
+ * Given a valid image file, load the image into the canvas
+ */
 loadImage = (file) => {
     const url = window.URL.createObjectURL(file);
     const img = new Image();
@@ -13,8 +19,31 @@ loadImage = (file) => {
         img.style.display = 'none';
         window.URL.revokeObjectURL(this.src);
 
-        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-        console.log(imageData);
-    }
-  
+        // count pixels per unique color
+        const color_count = this.countPixels(img, context);
+        
+        console.log(color_count);
+    }  
 };
+
+/**
+ * Counts the number of pixels per a unique color
+ */
+countPixels = (image, context) => {
+    const color_count = {};
+    for(let x = 0; x < image.width; x++) {
+        for(let y = 0; y < image.height; y++) {
+            const pixel = context.getImageData(x, y, 1, 1);
+            const data = pixel.data;
+            const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${(data[3] / 255)})`;
+
+            if (rgba in color_count) {
+                color_count[rgba] += 1;
+            } else {
+                color_count[rgba] = 1;
+            }
+        }
+    }
+
+    return color_count;
+}
